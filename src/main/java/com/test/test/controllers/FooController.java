34b091358d178c7011f2bd251425feddbed3182f
erickAC <cerickandrade@gmail.com>
2024-12-19 07:04:58 -0300

    adicionado retry e rate limiter

---
+++ src/main/java/com/test/test/controllers/FooController.java
@@ -0,0 +1,34 @@
+package com.test.test.controllers;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("foo")
+public class FooController {
+
+    Logger logger = LoggerFactory.getLogger(FooController.class);
+
+    @RateLimiter(name = "backendA", fallbackMethod = "fallback")
+    @Retry(name = "backendA", fallbackMethod = "fallback")
+    @GetMapping
+    public String hello() throws Exception {
+        // String response = RestClient.create("http://localhost:8080/teste").get().retrieve().body(String.class);
+        // return response;
+        return "Hello World";
+    }
+
+    public String fallback(Exception e) {
+        logger.info("Event Fallback");
+        return "Erro " + e.getMessage();
+    }
+
+}
